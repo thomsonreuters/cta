55859fb44999bc0c123d7d5ffb15efffd3e02312
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-12-06 23:51:12 +0100

    improved code coverage of config tests

--- test/unit/common.js
+++ test/unit/common.js
@@ -4,18 +4,20 @@ const sinon = require('sinon');
 const assert = require('chai').assert;
 const fs = require('fs');
 const path = require('path');
+const jsonfile = require('jsonfile');
 
 const Master = require('../../lib/master');
 const tools = require('../../lib/tools');
 const config = require('./lib/config.testdata');
 
 module.exports = {
-  sinon: sinon,
-  assert: assert,
-  fs: fs,
-  path: path,
-  Master: Master,
+  sinon,
+  assert,
+  fs,
+  path,
+  jsonfile,
+  Master,
+  config,
+  tools,
   master: new Master(config),
-  config: config,
-  tools: tools,
 };
