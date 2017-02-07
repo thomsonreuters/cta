afd1753383ded013a048d7e65265b2f3561d81d3
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2017-02-07 12:19:37 +0100

    disabled unit tests from CI

--- test/unit/lib/master.autoupdate.test.js
+++ test/unit/lib/master.autoupdate.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const o = require('../common');
+const requireSubvert = require('require-subvert')(__dirname);
 
 describe('cta - master - autoUpdate', function() {
   beforeEach(function() {
@@ -11,20 +12,49 @@ describe('cta - master - autoUpdate', function() {
     o.master.logger.log.restore();
   });
 
-  it('pull', function(done) {
-    const service = { git: o.git };
-    o.sinon.stub(service, 'git', () => {
+  it('should log error when it fails', function(done) {
+    const fn = () => {
       return {
         pull: (cb) => {
           cb('some_error', null);
         },
       };
-    });
-    o.master.autoUpdate()
-      .then(() => {
+    };
+    requireSubvert.subvert('simple-git', fn);
+    const Master = requireSubvert.require('../../../lib/master');
+    const master = new Master(o.config);
+    master.autoUpdate()
+      .then((data) => {
+        // console.log('data: ', data);
+        o.assert.strictEqual(data, 'some_error');
         done();
       })
       .catch((err) => {
+        // console.log('error: ', err);
+        done(err);
+      });
+  });
+
+  it('should retry when twice when it could not read from remote repository', function(done) {
+    const fn = () => {
+      return {
+        pull: (cb) => {
+          cb('Could not read from remote repository', null);
+        },
+      };
+    };
+    requireSubvert.subvert('simple-git', fn);
+    const Master = requireSubvert.require('../../../lib/master');
+    const master = new Master(o.config);
+    master.autoUpdate()
+      .then((data) => {
+        // console.log('data: ', data);
+        o.assert.strictEqual(data, 'Could not read from remote repository');
+        // TODO improve this test to track all calls to method autoUpdate
+        done();
+      })
+      .catch((err) => {
+        // console.log('error: ', err);
         done(err);
       });
   });
